a322d49438d67230f1c4129b10968a4586cdde67
langattr-cloud <langattr@gmail.com>
2026-08-15 14:50:26 +0300

    feat: load theme token overrides

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 import './aurora.css'
+import './theme-overrides.css'
 import { registerServiceWorker } from './lib/pwa'
 
 createRoot(document.getElementById('root')!).render(
