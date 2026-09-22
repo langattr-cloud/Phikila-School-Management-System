5e47d058df5d64e0d7f982efcbdfc86954ad3fed
langattr-cloud <langattr@gmail.com>
2026-09-22 20:50:10 +0300

    test(scheduling): align build input filter coverage

--- tests/test_scheduling_solver_build_input.py
+++ tests/test_scheduling_solver_build_input.py
@@ -1,11 +1,9 @@
 from app.modules.scheduling.engine import build_input
 
 
-def test_build_input_accepts_only_supported_filters():
+def test_build_input_exposes_generation_scope_filters():
     import inspect
 
     parameters = inspect.signature(build_input).parameters
-    assert "max_seconds" in parameters
-    assert "class_ids" not in parameters
-    assert "teacher_ids" not in parameters
-    assert "period_indexes" not in parameters
+    for name in ("max_seconds", "day_indexes", "period_indexes", "class_ids", "teacher_ids"):
+        assert name in parameters
